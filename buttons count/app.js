e9878429f37b8e7f0d1f3eba102624c56cc3225e
jesse <jessebijlsma151@hotmail.com>
2022-03-16 15:58:39 +0100

    update v0.2

---
+++ buttons count/app.js
@@ -0,0 +1,28 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+const buttons = document.querySelectorAll(".btn")
+const result = document.getElementById("res")
+const container = document.getElementById("container")
+let count = document.getElementById("count")
+
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        console.log(Math.random())
+    })
+};
+
+
+
+// container.textContent = numbers
+
+
+// function randomNumberReset() {
+//     return Math.floor(Math.random() *numbers. length);
+    // console.log(randomNumberReset)
+// }
+
+
+// if (count === 100){
+//     let reset = 0;
+// }
