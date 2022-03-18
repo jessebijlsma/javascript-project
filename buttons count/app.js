147b7a38e8a6cb06c5e43bf1213f9a6c501cefb1
jesse <jessebijlsma151@hotmail.com>
2022-03-18 01:42:22 +0100

    v0.3

--- buttons count/app.js
+++ buttons count/app.js
@@ -1,6 +1,6 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-const buttons = document.querySelectorAll(".btn")
+const buttons = document.querySelectorAll(".btn") 
 const result = document.getElementById("res")
 const container = document.getElementById("container")
 let count = document.getElementById("count")
@@ -8,10 +8,17 @@ let count = document.getElementById("count")
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        console.log(Math.random())
+        this.style.backgroundColor = "yellow"
+    // buttons[i].style.backgroundColor = "yellow";
+
+    // if (this.style.backgroundColor === 'yellow'){
+    //     this.style.backgroundColor = 'red'
+    //   } 
+    console.log("hello world")
+        
     })
 };
-
+console.log(buttons)
 
 
 // container.textContent = numbers
